d8c3a23b5f3e40a4ee8776ac9c7cac84102cf88d
Artem <kazuta.artem@gmail.com>
2022-06-23 16:19:55 +0300

    final-2

--- source/js/index.js
+++ source/js/index.js
@@ -1,5 +1,14 @@
 let map = document.querySelector(".map__image-map");
 let iframe = document.querySelector("iframe");
+let button = document.querySelector(".navigation__button");
+let menu = document.querySelector(".navigation__list-menu");
+
+button.addEventListener("click", open_menu);
+
+function open_menu() {
+  button.classList.toggle("navigation__button--toggle");
+  menu.classList.toggle("navigation__list-menu--open")
+}
 
 map.style.display = "none";
 iframe.style.display = "block";
