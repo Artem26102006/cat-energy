afb068395c53c8a8c129acfedf5499dfa7cc2aa9
Artem <kazuta.artem@gmail.com>
2022-06-29 10:07:51 +0300

    alalalal

--- source/js/index.js
+++ source/js/index.js
@@ -1,13 +1,18 @@
 let map = document.querySelector(".map__image-map");
 let iframe = document.querySelector("iframe");
-let button = document.querySelector(".navigation__button");
-let menu = document.querySelector(".navigation__list-menu");
+let button = document.querySelector(".navigation__button-menu");
+let menu = document.querySelector(".navigation__list-menu--open");
+let link = document.querySelector(".main-header__link")
+
+link.classList.remove("main-header__link--bottom")
+button.classList.add("navigation__button")
+menu.classList.toggle("navigation__list-menu")
 
 button.addEventListener("click", open_menu);
 
 function open_menu() {
   button.classList.toggle("navigation__button--toggle");
-  menu.classList.toggle("navigation__list-menu--open")
+  menu.classList.toggle("navigation__list-menu")
 }
 
 map.style.display = "none";
