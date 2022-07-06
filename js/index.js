54dc062a12d9df9d04325ba4be1a8bee65ad845e
Artem <kazuta.artem@gmail.com>
2022-07-06 10:44:23 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -2,7 +2,11 @@ let map = document.querySelector(".map__image-block");
 let iframe = document.querySelector("iframe");
 let button = document.querySelector(".navigation__button-menu");
 let menu = document.querySelector(".navigation__list-menu-open");
-let link = document.querySelector(".main-header__link")
+let link = document.querySelector(".main-header__link");
+let cat_before = document.querySelector(".example__slider-picture-before");let cat_after = document.querySelector(".example__slider-picture-after");
+let button_before = document.querySelector(".slider-control__button--before");
+let button_after = document.querySelector(".slider-control__button--after");
+
 
 link.classList.remove("main-header__link--bottom")
 button.classList.add("navigation__button")
@@ -14,6 +18,21 @@ function open_menu() {
   button.classList.toggle("navigation__button--toggle");
   menu.classList.toggle("navigation__list-menu")
 }
+
+button_after.addEventListener("click", show_after);
+
+function show_after() {
+  cat_before.style.display = "none";
+  cat_after.style.display = "block";
+}
+
+button_before.addEventListener("click", show_before);
+
+function show_before() {
+  cat_before.style.display = "block";
+  cat_after.style.display = "none";
+}
+
 
 map.style.display = "none";
 iframe.style.display = "block";
